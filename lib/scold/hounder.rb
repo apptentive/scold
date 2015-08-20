dc2ab4d18efc72fc9283dc43e951b173a5331e04
John Fearnside <john@apptentive.com>
2015-08-20 11:31:38 -0700

    Use git status to gather files for hounding

--- lib/scold/hounder.rb
+++ lib/scold/hounder.rb
@@ -1,11 +1,18 @@
 module Scold
   class Hounder
+    CHANGED = %w(M A R C).freeze
+
     def initialize(args = [])
       @args = args.dup
     end
 
-    def call
-      files = `git diff-index --name-only master`.split($RS)
+    def call # rubocop:disable Metrics/AbcSize
+      files = `git status --porcelain`.lines.inject([]) do |accum, line|
+        if CHANGED.include?(line[0]) || CHANGED.include?(line[1])
+          accum << line[3..-2].split("->").last.strip
+        end
+        accum
+      end
       if files.empty?
         require "scold/exit"
         Exit::SUCCESS
